f5453168d258b5b8e329c3bab2dd0c9de1ba4a36
IShaLinI <mrsaltinecracker1@gmail.com>
2024-06-07 17:09:05 -0700

    added the clock in button

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,20 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![clock_in, clock_out])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+
+#[tauri::command]
+fn clock_in() -> () {
+  println!("Clocking in");
+  ()
+}
+
+#[tauri::command]
+fn clock_out() -> () {
+  println!("Clocking out");
+  ()
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+    import { invoke } from '@tauri-apps/api/tauri'
+
+    let clock_button_text = "Clock In";
+    let clocked_in = false;
+
+    function clock(){
+        if(clocked_in){
+            clock_out();
+        } else {
+            clock_in();
+        }
+    }
+
+    function clock_in(){
+        clock_button_text = "Clock Out";
+        clocked_in = true;
+        invoke('clock_in');
+    }
+
+    function clock_out(){
+        clock_button_text = "Clock In";
+        clocked_in = false;
+        invoke('clock_out');
+    }
+
+
+</script>
+
+<style>
+    body {
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        width: 100vw;
+        height: 100vh;
+        padding: 0;
+        margin: 0;
+    }
+</style>
+<body>
+    <button on:click={clock}>{clock_button_text}</button>
+</body>
